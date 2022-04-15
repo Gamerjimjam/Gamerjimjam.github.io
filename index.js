ea9ce5c900b3ce4d688b153ecbec1c10b38b6d8d
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-04-15 18:14:49 -0400

    this wont work for sure but its nice to try

--- index.js
+++ index.js
@@ -4,14 +4,3 @@
     scrolltitle(text.substr(3) + text.substr(0, 3));
 }, 500);
 }("who really is gamerjimjam and why is he so ass at coding?"));
-
-(function rainbowtext(){
-    var colors = ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#4B0082", "#8F00FF"];
-    var i = 0;
-    setInterval(function(){
-        document.getElementById("rainbow").style.color = colors[i];
-        i++;
-        if(i >= colors.length) i = 0;
-    }, 500);
-}());
-// i pray to god that this works
