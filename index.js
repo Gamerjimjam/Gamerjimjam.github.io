17db2631d5d24a10b83c8faa6014288f2552d996
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-04-15 15:33:49 -0400

    cool title thing

---
+++ index.js
@@ -0,0 +1,6 @@
+(function scrolltitle(text){
+    document.title = text;
+    setTimeout(function(){
+        scrolltitle(text.substr(1) + text.substr(0, 1));
+    }, 500);
+}("who really is gamerjimjam and why is he so ass at coding?"));
