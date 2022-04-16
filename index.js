b6bce3a6c0c0bc85ef4c539e4477c42d7c98c314
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-04-16 11:03:22 -0400

    if the js thing works, then ai is taking over

--- index.js
+++ index.js
@@ -4,3 +4,14 @@
     scrolltitle(text.substr(3) + text.substr(0, 3));
 }, 500);
 }("who really is gamerjimjam and why is he so ass at coding?"));
+
+
+var facts = ["every second we are inching closer to death","AMONG US???","im bored","i shouldve gotten an ai to generate facts","totally tubular dude 🤙","facts dont care about your feelings"]
+
+(function fact(){
+    var random = Math.floor(Math.random() * facts.length);
+    document.getElementById("fact").innerHTML = facts[random];
+    setTimeout(function(){
+    fact();
+}, 5000);
+}());
