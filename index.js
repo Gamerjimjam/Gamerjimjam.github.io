a5f146b4a20f82e68f7a45b11787d1d867d14a19
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-04-16 14:47:08 -0400

    hm

--- index.js
+++ index.js
@@ -8,36 +8,9 @@
 
 var facts = ["every second we are inching closer to death","AMONG US???","im bored","i shouldve gotten an ai to generate facts","totally tubular dude 🤙","facts dont care about your feelings"]
 
-// displays random fact every 5 seconds
-// when every fact is displayed it enters word by word
+// displays random fact every time the page is reloaded
+function randomFact() {
+    var randomNumber = Math.floor(Math.random() * facts.length);
+    document.getElementById("fact").innerHTML = facts[randomNumber];
+}
 
-
-
-// displays random fact every 5 seconds
-// when every fact is displayed it enters word by word
-
-fact_displayer = () => {
-    //I added this line to make sure #fact is empty before I overwrite its content
-    document.getElementById("fact").innerHTML='';
-  
-    var fact = facts[Math.floor(Math.random() * facts.length)];
-  
-    var i = 8;
-  
-    var timer = setInterval(function() {
-      document.getElementById("fact").innerHTML += fact[i];
-      i++;
-  
-      if (i >= fact.length) {
-  
-        clearInterval(timer);
-  
-        setTimeout(() => {
-          document.getElementById("fact").innerHTML = "";
-          setTimeout(() => {
-            scrolltitle(fact);
-         }, 1000);
-        }, 1000);
-      }
-    }, 100);
-  }
