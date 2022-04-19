19cde86c1ca23eed8801342ef511be4005623480
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-04-19 11:25:45 -0400

    ikd

--- index.js
+++ index.js
@@ -8,9 +8,20 @@
 
 var facts = ["every second we are inching closer to death","AMONG US???","im bored","i shouldve gotten an ai to generate facts","totally tubular dude 🤙","facts dont care about your feelings"]
 
-(function displayfct(text){
-    document.getElementById("facts").innerHTML = text;
-    setTimeout(function(){
-    displayfct(text.substr(3) + text.substr(0, 3));
-}, 500);
-}(facts[Math.floor(Math.random() * facts.length)]));
+(function displayfctwhenbutton(text){
+    document.getElementBYId("button").onclick = function(){
+        // makes fact appear word by word
+        var i = 0;
+        var txt = facts[i];
+        var speed = 50;
+        var x = setInterval(function(){
+            if(i < facts.length){
+                document.getElementById("fact").innerHTML += txt.charAt(i);
+                i++;
+            }
+            else{
+                clearInterval(x);
+            }
+        }, speed);
+    }
+}());
