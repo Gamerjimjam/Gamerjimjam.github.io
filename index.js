e807f1377bac95240ad47239c040d760d148ac80
Gamerjimjam <85574259+Gamerjimjam@users.noreply.github.com>
2022-04-19 10:48:19 -0400

    im onna get bullied by peope on stack exchage

--- index.js
+++ index.js
@@ -8,12 +8,9 @@
 
 var facts = ["every second we are inching closer to death","AMONG US???","im bored","i shouldve gotten an ai to generate facts","totally tubular dude 🤙","facts dont care about your feelings"]
 
-// displays random fact every time the page is reloaded
-function randomFact() {
-    var randomNumber = Math.floor(Math.random() * facts.length);
-    document.getElementById("fact").innerHTML = facts[randomNumber];
-    // adds scroll frame to the text so it looks cool
-    scrolltitle(randomNumber);
-}
-// calls function every page reload
-randomFact();
+(function displayfct(text){
+    document.getElementById("facts").innerHTML = text;
+    setTimeout(function(){
+    displayfct(text.substr(3) + text.substr(0, 3));
+}, 500);
+}(facts[Math.floor(Math.random() * facts.length)]));
